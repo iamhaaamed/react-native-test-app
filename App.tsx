4f4ed7efa38750bdb1ac783409f9fb3c039158c2
Hamed <hamed.azadegan@gmail.com>
2024-01-25 06:56:21 +0330

    feat: add api provider

--- App.tsx
+++ App.tsx
@@ -1,25 +1,22 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {StyleSheet} from 'react-native';
+
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {hydrateAuth} from './src/core';
 import {RootNavigator} from './src/navigation';
+import {APIProvider} from './src/api';
 
 hydrateAuth();
 
 const App = () => {
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <RootNavigator />
+    <GestureHandlerRootView className="flex-1">
+      <APIProvider>
+        <RootNavigator />
+      </APIProvider>
     </GestureHandlerRootView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
